b12206b04f76637e49a6c7a2328fcc7c7490b03d
Aura <aura@openroadtms.com>
2022-06-24 15:09:38 +0300

    content accessible only with login

--- front/src/App.js
+++ front/src/App.js
@@ -30,7 +30,12 @@ function App() {
     fetch('http://localhost:3001/api/v1/auth/signup', requestOptions)
       .then(response => response.json())
       .then(data => {
-        localStorage.setItem('Token', data.token);
+        console.log(data)
+        if (typeof Storage !== "undefined") {
+          localStorage.setItem('Token', data.token);
+        } else {
+          alert('Sorry! No web storage support...')
+        }
       });
   }
 
@@ -49,7 +54,8 @@ function App() {
     fetch('http://localhost:3001/api/v1/auth/login', requestOptions)
       .then(response => response.json())
       .then(data => {
-        localStorage.getItem('Token');
+        console.log(data)
+        localStorage.setItem('Token', data.token);
         setCurrentUser(data.data.user)
         setRender(prevState => !prevState)
       });
@@ -57,29 +63,28 @@ function App() {
 
   function clearUser() {
     setCurrentUser('')
-    localStorage.clear();
+    localStorage.removeItem('Token');
   }
 
+  useEffect(() => {
+    if (localStorage.Token === undefined) {
 
-  // useEffect(() => {
-    // if (localStorage.user === undefined) {
-    //  No access
-    // } else { } 
-  //   const requestOptions = {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       "Authorization": `Bearer ${'22simboli'}`
-  //     }
-  //   };
-  //   fetch('http://localhost:3001/api/v1/auth', requestOptions)
-  //     .then(response => response.json())
-  //     .then(result => {
-  //       getUsers(result.data.user)
-  //       localStorage.clear();
-  //     });
-
-  // }, [render])
+    } else {
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('Token')}`
+        }
+      };
+      fetch('http://localhost:3001/api/v1/auth', requestOptions)
+        .then(response => response.json())
+        .then(res => {
+          console.log(res.data.user)
+          getUsers(res.data.user)
+        });
+    }
+  }, [render])
 
 
   return (
@@ -89,7 +94,7 @@ function App() {
         <input type="text" onChange={(e) => { setUsername(e.target.value) }} placeholder='username'></input>
         <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder='email'></input>
         <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder='password'></input>
-        <input type="confPassword" onChange={(e) => { setConfPassword(e.target.value) }} placeholder='confirm password'></input>
+        <input type="password" onChange={(e) => { setConfPassword(e.target.value) }} placeholder='confirm password'></input>
         <input type="submit" value="register"></input>
       </form>
 
@@ -103,20 +108,41 @@ function App() {
       <button onClick={(e) => clearUser(e)}>Log out</button>
 
 
-      {!currentUser ? <h6>User not logged in</h6> : <h3>Hello, {currentUser.username}</h3>}
+      {!currentUser ? (
+        <>
+          <h6>User not logged in</h6>
+          <h6 className='content'>Log in to get access</h6>
+        </>
+      ) : (
+        <>
+          <h3>Hello, {currentUser.username}</h3>
+          <div>
+            {users.map((data) =>
+              <ul key={data._id}>
+                <li>{data._id}</li>
+                <br />
+                <li>{data.username}</li>
+                <li>{data.email === undefined ? 'No email' : data.email}</li>
+                <li>{data.type}</li>
+              </ul>
+            )}
+          </div>
+        </>
+      )}
 
-      {users.map((data) =>
-        <ul key={data._id}>
-          <li>{data._id}</li>
-          <br />
-          <li>{data.username}</li>
-          <li>{data.email === undefined ? 'No email' : data.email}</li>
-          <li>{data.type}</li>
-        </ul>
-      )
-      }
+      {/* <div id='content'>
+        {users.map((data) =>
+          <ul key={data._id}>
+            <li>{data._id}</li>
+            <br />
+            <li>{data.username}</li>
+            <li>{data.email === undefined ? 'No email' : data.email}</li>
+            <li>{data.type}</li>
+          </ul>
+        )}
+      </div> */}
     </div>
-  );
+  )
 }
 
 export default App;
